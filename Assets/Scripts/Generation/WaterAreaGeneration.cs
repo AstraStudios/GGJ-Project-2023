36d50d5cc794925f820ce86a86dfed972a88c390
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-02-04 15:04:53 -0600

    Water area gen

---
+++ Assets/Scripts/Generation/WaterAreaGeneration.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WaterAreaGeneration : MonoBehaviour
+{
+    [SerializeField] TMP_Text waterAmountText;
+    [SerializeField] GameObject waterArea;
+    float randomY;
+    float randomX;
+
+    public int amountWithWat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            randomY = Random.Range(0, -32);
+            randomX = Random.Range(-19.4f, 19.4f);
+            Instantiate(waterArea, new Vector3(randomX, randomY, 0), Random.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckWaterAmount()
+    {
+        amountWithWat = 15;
+    }
+}
