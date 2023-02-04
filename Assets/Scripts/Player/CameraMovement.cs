1467e59035deea6574490523b29b2685cdd42579
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-02-04 12:38:44 -0600

    TMP and Movement
    Co-Authored-By: sustachio <88939526+sustachio@users.noreply.github.com>

---
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(0, y);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
