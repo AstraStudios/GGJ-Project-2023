eeece0873132bf9dbcfa32e827def84cc8dcadd8
sustachio <sustachio@gmail.com>
2023-02-05 10:53:51 -0600

    Create Plant Grow On click script

---
+++ Assets/Scripts/Generation/PlantGrowOnClick.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGrowOnClick : MonoBehaviour
+{
+    // the script to generate plants
+    PlantLSystem tree;
+    BoxCollider2D boxCollider;
+
+    private void ResizeColider()
+    {
+        // calculate the size of the tree
+        Vector2 size;
+        size.x = tree.maxX - tree.minX;
+        size.y = tree.maxY - tree.minY;
+
+        boxCollider.size = size;
+
+        // get the offset
+        Vector2 offset;
+        offset.x = tree.minX + (size.x / 2);
+        offset.y = tree.minY + (size.y / 2);
+
+        boxCollider.offset = offset;
+    }
+
+    private void Start()
+    {
+        tree = gameObject.GetComponent<PlantLSystem>();
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        ResizeColider();
+    }
+
+    // make the tree bigger
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked!");
+        tree.Generate(tree.currentRecusrionLevel + 1);
+    }
+}
