d6dc31fd4d216aee19dc3cca3adf3f5d2dbd2622
sustachio <sustachio@gmail.com>
2023-02-05 13:46:19 -0600

    Update water generator

--- Assets/Scripts/Generation/WaterAreaGeneration.cs
+++ Assets/Scripts/Generation/WaterAreaGeneration.cs
@@ -11,22 +11,15 @@ public class WaterAreaGeneration : MonoBehaviour
     float randomX;
     int randomSpawnAmount;
 
-    // Start is called before the first frame update
     void Start()
     {
-        randomSpawnAmount = Random.Range(50, 125);
+        randomSpawnAmount = 100;
         for (int i = 0; i < randomSpawnAmount; i++)
         {
-            randomY = Random.Range(-10, -1000);
+            randomY = Random.Range(-5, -1000);
             randomX = Random.Range(-19.4f, 19.4f);
             GameObject water = Instantiate(waterArea, new Vector3(randomX, randomY, 0), Quaternion.Euler(0, 0, Random.Range(0, 360)));
             water.transform.parent = waterParent.transform;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
