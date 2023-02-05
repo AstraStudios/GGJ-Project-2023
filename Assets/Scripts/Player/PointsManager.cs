ef2abddca4c0d10c96a427c4ec18d4823b29a841
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-02-05 13:45:21 -0600

    Pointsmanager money works

--- Assets/Scripts/Player/PointsManager.cs
+++ Assets/Scripts/Player/PointsManager.cs
@@ -13,21 +13,24 @@ public class PointsManager : MonoBehaviour
     [SerializeField] TMP_Text waterAmountText;
     [SerializeField] TMP_Text sunlightSpendAmountText;
     [SerializeField] TMP_Text waterSpendAmountText;
-    [SerializeField] TMP_Text buyingPanelSpendAmountText;
-    [SerializeField] GameObject buyPanel;
+    [SerializeField] TMP_Text notEnoughMoneyText;
 
     float ambientWater; // add constantly
     float ambientSunlight; // add constantly
     public int sunlightSpendAmount;
     public int waterSpendAmount;
     int plantAmount;
+    int recusrionLevel;
     public float waterAmount;
     public float sunlightAmount;
+    public int priceSunlight;
+    public int priceWater;
 
     public float totalMoney;
-    public bool didBuy;
 
     GameObject[] numOfPlantsInScene;
+    [SerializeField] GameObject growOnClickSystemObject;
+    PlantGrowOnClick plantGrowOnClickScript;
     RootsManager rootsManagerScript;
 
     // Start is called before the first frame update
@@ -38,11 +41,13 @@ public class PointsManager : MonoBehaviour
         waterSpendAmount = 0;
         waterAmount = 0;
         sunlightAmount = 0;
+        ambientWater = 10;
+        plantGrowOnClickScript = growOnClickSystemObject.GetComponent<PlantGrowOnClick>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         AmountOfPlants();
         SunlightCalc();
         WaterCalc();
@@ -60,7 +65,7 @@ public class PointsManager : MonoBehaviour
     void SunlightCalc()
     {
         // Calculate the sunlight per second
-        ambientSunlight += 0.0015f * plantAmount * Time.deltaTime;
+        ambientSunlight += 0.0015f * recusrionLevel * Time.deltaTime;
         sunlightAmount += ambientSunlight;
         sunlightAmountText.text = Mathf.RoundToInt(sunlightAmount).ToString();
     }
@@ -68,7 +73,8 @@ public class PointsManager : MonoBehaviour
     void WaterCalc()
     {
         // calculate water from holes and ambient
-        ambientWater += .0005f * Time.deltaTime;
+        ambientWater += .5f * rootsManagerScript.totalRootPerimeter * Time.deltaTime;
+        ambientWater = ambientWater / 2;
         waterAmount += ambientWater; // add water from holes later
         waterAmountText.text = Mathf.RoundToInt(waterAmount).ToString();
     }
@@ -76,7 +82,7 @@ public class PointsManager : MonoBehaviour
     void LengthCalc()
     {
         // 1 sunlight every meter
-        sunlightSpendAmount = Mathf.RoundToInt(rootsManagerScript.currentRootLength / 2);
+        sunlightSpendAmount = Mathf.RoundToInt(rootsManagerScript.currentRootLength * 2);
         waterSpendAmount = sunlightSpendAmount / 2;
         waterSpendAmountText.text = Mathf.RoundToInt(waterSpendAmount).ToString();
         sunlightSpendAmountText.text = Mathf.RoundToInt(sunlightSpendAmount).ToString();
@@ -84,9 +90,40 @@ public class PointsManager : MonoBehaviour
 
     public void PayForLine()
     {
-        Debug.Log("Hello?");
         Debug.Log("Transcation starting");
-        sunlightAmount -= sunlightSpendAmount;
-        waterAmount -= waterSpendAmount;
+        if (sunlightAmount > sunlightSpendAmount && waterAmount > waterSpendAmount)
+        {
+            sunlightAmount -= sunlightSpendAmount;
+            waterAmount -= waterSpendAmount;
+            Debug.Log("Transcation complete!");
+        }
+        else
+        {
+            PlaceText();
+        }
+    }
+    IEnumerator PlaceText()
+    {
+        for (; ;)
+        {
+            notEnoughMoneyText.text = "You do not have enough money!";
+        }
+        yield return new WaitForSeconds(.5f);
+    }
+
+    public void PayForTree()
+    {
+        switch (recusrionLevel)
+        {
+            case 1:
+                priceSunlight += 30;
+                priceWater += 15;
+                break;
+            case 2:
+                priceSunlight += 60;
+                priceWater += 30;
+                break;
+
+        }
     }
 }
